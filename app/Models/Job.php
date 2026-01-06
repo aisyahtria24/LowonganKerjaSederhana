6e48f30d4529e77c5f7da98e07c7185d8c953dc6
khalifanayna <khalifa24ti@mahasiswa.pcr.ac.id>
2026-01-06 11:38:26 +0700

    5.2

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'company_name',
+        'location',
+        'work_system',
+        'job_type',
+        'salary_range',
+        'category',
+        'status',
+    ];
 }
